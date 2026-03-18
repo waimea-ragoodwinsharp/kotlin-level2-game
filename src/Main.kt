fa1c5acfdbc259bbf14b7bf4350123ce890bb5ed
waimea-ragoodwinsharp <ragoodwinsharp@waimea.school.nz>
2026-03-19 12:14:10 +1300

    Updated main

--- src/Main.kt
+++ src/Main.kt
@@ -2,9 +2,9 @@
  * =====================================================================
  * Programming Project for NCEA Level 2, Standard 91896
  * ---------------------------------------------------------------------
- * Project Name:   PROJECT NAME HERE
- * Project Author: PROJECT AUTHOR HERE
- * GitHub Repo:    GITHUB REPO URL HERE
+ * Project Name:   The Squeeze Level 2 game
+ * Project Author: Rose Goodwin-Sharp
+ * GitHub Repo:    https://github.com/waimea-ragoodwinsharp/kotlin-level2-game
  * ---------------------------------------------------------------------
  * Notes:
  * PROJECT NOTES HERE
@@ -14,6 +14,6 @@
 
 
 fun main() {
-    println("Hello World!")
+
 }
 
