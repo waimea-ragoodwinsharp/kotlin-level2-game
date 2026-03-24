85eec9028299048b83f9174a9d127a4d0ffc6b1f
waimea-ragoodwinsharp <ragoodwinsharp@waimea.school.nz>
2026-03-24 13:26:04 +1300

    Fixed board and asked for player names

--- src/Main.kt
+++ src/Main.kt
@@ -12,27 +12,55 @@
  */
 
 val squares = mutableListOf<String>()
+val p1Name = "unknown"
+val p2Name = "unknown"
+
 fun main() {
    println("⊹₊ ˚‧︵‿₊୨ ᰔ ୧₊‿︵‧ ˚ ₊⊹".red())
     println("┌-------------------┐".magenta())
     println("│   The Squeeze     │".magenta())
     println("└-------------------┘".magenta())
-createSquares()
-    addingPlayers()
+    setupBoard()
+    getPlayerNames()
 
     while (true){
         showSquares()
+        // get the p1's move
+
+        // make the move on the board
+
+
+        // Get P2 move
+
+        // Make it
+
+        // Squish!
 
         break
     }
 
 }
 //Making the squares exits
-fun createSquares() {
+fun setupBoard() {
     repeat(15) {
         squares.add("EMPTY")
     }
+
+    squares[5] = "P1"
+
+    squares[6] = "P2"
+
+    squares[7] = "P1"
+
+    squares[8] = "P2"
+
+    squares[9] = "P1"
+
+    squares[10] = "P2"
+
 }
+
+
 // showing the squares and making them pretty
 fun showSquares() {
 
@@ -43,7 +71,7 @@ fun showSquares() {
 
     println("+--------------".repeat(squares.size).blue() + "+".blue())
 
-    for (i in 0..<squares.size) {
+    for (i in 1..<squares.size) {
         print("| ${squares[i].padEnd(12)} ".cyan())
     }
     println("|".blue())
@@ -51,23 +79,21 @@ fun showSquares() {
     println("+--------------".repeat(squares.size).blue() + "+".blue())
 
 }
-// I AM SO CONFUUSSEEDD
-
-fun addingPlayers() {
-
-var player = thePlayers
-    for (i in ) {
-        if (squares[i] == "EMPTY") {
-           squares[i] = player
-            break
-        }
-    }
-}
 
 
-fun thePlayers() {
+fun getPlayerNames() {
+    println("Player 1, what is your name? ")
+    var p1name = readlnOrNull()
+    if (p1name.isNullOrBlank())
+        p1name = "Player 1"
+    println("Player 2, what is your name? ")
+    var p2name = readlnOrNull()
+    if (p2name.isNullOrBlank())
+        p2name = "Player 2"
 
 }
+
+
 
 
 
