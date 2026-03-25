4d9615ee04ff8f74bea4074d06edd2343220068c
waimea-ragoodwinsharp <ragoodwinsharp@waimea.school.nz>
2026-03-26 12:26:33 +1300

    Fixed board and player names

--- src/Main.kt
+++ src/Main.kt
@@ -12,17 +12,19 @@
  */
 
 val squares = mutableListOf<String>()
-val p1Name = "unknown"
-val p2Name = "unknown"
+var p1Name = "P1"
+var p2Name = "P2"
 
 fun main() {
    println("⊹₊ ˚‧︵‿₊୨ ᰔ ୧₊‿︵‧ ˚ ₊⊹".red())
     println("┌-------------------┐".magenta())
     println("│   The Squeeze     │".magenta())
     println("└-------------------┘".magenta())
+
     setupBoard()
     getPlayerNames()
-    getPlayerAction()
+//  getPlayerAction1()
+//  getPlayerAction2()
     switchSquares()
 
 
@@ -41,31 +43,32 @@ fun main() {
 
         break
     }
-    while (true) {
-        val action = getPlayerAction()
-        when (action) {
-
-        }
-    }
+//    while (true) {
+//        val action = getPlayerAction1()
+//        val action = getPlayerAction2()
+//        when (action) {
+//
+//        }
+//    }
 
 }
 //Making the squares exist
 fun setupBoard() {
     repeat(15) {
-        squares.add("EMPTY")
+        squares.add("     ")
     }
 
-    squares[5] = "P1"
+    squares[4] = "P1"
 
-    squares[6] = "P2"
+    squares[5] = "P2"
 
-    squares[7] = "P1"
+    squares[6] = "P1"
 
-    squares[8] = "P2"
+    squares[7] = "P2"
 
-    squares[9] = "P1"
+    squares[8] = "P1"
 
-    squares[10] = "P2"
+    squares[9] = "P2"
 
 
 
@@ -76,54 +79,64 @@ fun setupBoard() {
 fun showSquares() {
 
     for (i in 1..squares.size) {
-        print(" Square $i      ".yellow())
+        print("   ${i.toString().padStart(2)}   ".yellow())
     }
     println()
 
-    println("+--------------".repeat(squares.size).blue() + "+".blue())
+    println("+-------".repeat(squares.size).blue() + "+".blue())
 
-    for (i in 1..<squares.size) {
-        print("| ${squares[i].padEnd(12)} ".cyan())
+    for (i in 0..<squares.size) {
+        print("| ${squares[i].padEnd(5)} ".cyan())
     }
     println("|".blue())
 
-    println("+--------------".repeat(squares.size).blue() + "+".blue())
+    println("+-------".repeat(squares.size).blue() + "+".blue())
 
 }
 
 
 fun getPlayerNames() {
     println("Player 1, what is your name? ")
-    var p1name = readlnOrNull()
-    if (p1name.isNullOrBlank())
-        p1name = "Player 1"
-    println("Player 2, what is your name? ")
-    var p2name = readlnOrNull()
-    if (p2name.isNullOrBlank())
-        p2name = "Player 2"
-
+    val answer = readlnOrNull()
+    if (!answer.isNullOrBlank()) {
+            p1Name = answer
+            break
+        }
 }
+        while (true) {
+    val p2Name = readlnOrNull()
+    if (p2Name != null && p2Name.isNotBlank()) break
+
+
+//    var p1name = readlnOrNull()
+//    if (p1name.isNullOrBlank())
+//        p1name = "Player 1"
+//    println("Player 2, what is your name? ")
+//    var p2name = readlnOrNull()
+//    if (p2name.isNullOrBlank())
+//        p2name = "Player 2"
+
+
 
 fun switchSquares() {
-    println("Player 1, Choose a square to switch to")
-    var squareNum1 = 1
-    var squareNum2 = 2
+    println("$p1Name, Choose a square to switch to")
 
-    val temp = squareNum1
-    squareNum1 = squareNum2
-    squareNum2 = temp
 
 
 
 }
 
 
-fun getPlayerAction(): Char {
-    println("Alright $p1Name. Choose your move")
-
-}
-
-
+//fun getPlayerAction1(): Char {
+//    println("Alright $p1Name. Choose your move")
+//
+//}
+//
+//
+//fun getPlayerAction2(): Char {
+//    println("Alright ${p2Name}Name. Choose your move")
+//
+//}
 
 
 
